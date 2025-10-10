4f807c13b2bc90e0679a984b52fce4fae0f72427
AGFE\AgFe <duug@naver.com>
2025-10-10 19:03:07 +0900

    init: first commit for WedSnap project

---
+++ src/main/java/me/agfe/wedsnap/WedSnapApplication.java
@@ -0,0 +1,13 @@
+package me.agfe.wedsnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedSnapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WedSnapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/agfe/wedsnap/WedSnapApplicationTests.java
@@ -0,0 +1,13 @@
+package me.agfe.wedsnap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WedSnapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
